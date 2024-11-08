197e7f29d0ee49ce6aaad9815e8417ccb14a64f8
codekappal <captain@kappal.in>
2024-11-08 13:07:35 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -51,8 +51,9 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL || "redis://localhost:6379",  
     redisPrefix: process.env.REDIS_URL || "kappal:",
-    databaseDriverOptions: process.env.NODE_ENV !== "development" ?
-      { ssl: { rejectUnauthorized: false } } : {},
+    databaseDriverOptions: {},
+    // databaseDriverOptions: process.env.NODE_ENV !== "development" ?
+    //   { ssl: { rejectUnauthorized: false } } : {},
     sessionOptions: {
         name: process.env.SESSION_NAME || "kappal",
       },
